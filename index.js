50eef2f3b567c5393ba01dd21ac1c038ab68d766
lukachochua <lukachochua@gmail.com>
2023-03-09 16:53:40 +0400

    Episode 1-7: primitive data types

---
+++ index.js
@@ -0,0 +1,28 @@
+// primitive data types
+
+// STRINGS
+"hello";
+'hello';
+"he said \"hello\"";
+'hello world "yup"';
+"hello world 'yup";
+
+//NUMBERS
+// Two types of numbers  - integers and floats
+//int
+1;
+100;
+200;
+//float
+1.1;
+100.256;
+
+// BOOLEANS
+// float or sink, positive or negative, 0 or 1, on or off
+true;
+false;
+
+// OTHER PRIMITIVE TYPES - still a value that let's you know there's nothing in it
+null - 'no value assigned';
+undefined - 'same thing';
+NaN - 'Not A Number';
