8e0c88009cddc6a40a4c73dc68801ad39e801f6a
lukachochua <lukachochua@gmail.com>
2023-03-13 22:31:15 +0400

    Episode 13: objects and arrays

--- index.js
+++ index.js
@@ -1,21 +1,41 @@
-// Functions and Subroutines.
+/*
+Objects and Arrays
 
-// Functions are set of prewritten instructions for a computer to execute when they're called from the memory
-// Functions define subroutines, like routines inside humans minds. Programmer names it, gives it set of instructions and calls it when needed.
-// functions can have parameters, that are variables, that can have values asssigned to them when we execute instructions. 
-// Curly braces define functions body. Functions are called with return function.
+Objects are obvious for what they're. Arrays are list objects. Arrays are objects, but are type of objects.
 
-function makeCoffe(sugar, milk)
+Standart Object
 {
-    let instructions = 'Boil Water,'; // These variables inside functions are accessible only in the scope of the function.
-    instructions += " pour into cup,";
-    instructions += " add coffee granules,";
-    instructions += " add " + sugar + " spoons of sugar,";
-    instructions += " add " + milk + "% milk.";
-    
-    return instructions;
-} // Make coffee as a function example.
+     key    value  = properties | nouns are keys, adjectives are values.
+    color: "red",
+    speed: 165,
+    maker: volvo,
+    drive: function() {},
+    stop: function() {},
+};
 
-console.log(makeCoffe(2, 20));
+when you take subroutine or function and add it to the object, it's called method. Just to be clear when we're talking about objects.
 
-// If we mention parameters, or arguments, we know we're talking about invoking a function and/or function in General.
+Arrays. Arrays are 0 indexed. first item is indexed as 0. If you delete item of array, key names are reassigned.
+[
+    0: Apple,
+    1: Orange,
+    2: Pear
+]
+*/
+
+// Example of the Object
+
+let car = {
+    color : 'red',
+    speed : 200,
+    drive: function() { return "drive";},
+
+};
+
+// Example of the Array
+
+let shoppingList = [
+    'Apple',
+    'Orange',
+    'Pear'
+];
