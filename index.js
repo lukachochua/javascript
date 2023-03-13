12b4c9cf11da3ed0b15fffb9159465c7288c2578
lukachochua <lukachochua@gmail.com>
2023-03-13 22:52:31 +0400

    Episode 15: member access

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// Embedding Objects and Arrays
+// Member Access
+
+// Standart objects properties and/or methods are called objects members. Also with array -  each element is a member of an array.
 
 let car = {
     make: "Volvo",
@@ -12,7 +14,16 @@ let car = {
     drive: function() { return "drive"}
 };
 
-// There is hierarchy, kind of structure in which our object is created.
+// In javascript '.' (dot symbol) is dedicated to the member access.
+
+console.log(car.make);
+console.log(car.engine.make); // You can continue using dot syntax to get information from sub/member-objects of the initial object.
+console.log(car.drive()); // in order to run member function from an object, you need to use parenthesis() after function name. 
+// If you don't use parenthesis it will print out just the syntax of the function.
+// It's pretty familiar to console.log() method. We looking in the Console Object and using the log function. 
+
+console.log(car.engine.pistons[0]); // To access a member of an array you can use brackets [] and index of an array member.
+// It's called computed member access.
 
 let array = ["string",
     100, 
@@ -21,4 +32,3 @@ let array = ["string",
     function() { return "drive"}
 ];
 
-// Arrays can embed objects and objects can embed arrays
