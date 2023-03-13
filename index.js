e3ea271a29fa362158d8f8193eacb18ead7fa99e
lukachochua <lukachochua@gmail.com>
2023-03-13 22:40:55 +0400

    Episode 14: embedding objects and arrays

--- index.js
+++ index.js
@@ -1,41 +1,24 @@
-/*
-Objects and Arrays
-
-Objects are obvious for what they're. Arrays are list objects. Arrays are objects, but are type of objects.
-
-Standart Object
-{
-     key    value  = properties | nouns are keys, adjectives are values.
-    color: "red",
-    speed: 165,
-    maker: volvo,
-    drive: function() {},
-    stop: function() {},
-};
-
-when you take subroutine or function and add it to the object, it's called method. Just to be clear when we're talking about objects.
-
-Arrays. Arrays are 0 indexed. first item is indexed as 0. If you delete item of array, key names are reassigned.
-[
-    0: Apple,
-    1: Orange,
-    2: Pear
-]
-*/
-
-// Example of the Object
+// Embedding Objects and Arrays
 
 let car = {
-    color : 'red',
-    speed : 200,
-    drive: function() { return "drive";},
-
+    make: "Volvo",
+    speed: "160",
+    engine: {
+        size: 2.0,
+        make: "BMW",
+        fuel: "petrol",
+        pistons: [{maker: "BMW"}, {maker: "BMW2"}],
+    },
+    drive: function() { return "drive"}
 };
 
-// Example of the Array
+// There is hierarchy, kind of structure in which our object is created.
 
-let shoppingList = [
-    'Apple',
-    'Orange',
-    'Pear'
+let array = ["string",
+    100, 
+    ["embed", 200],
+    { car: "ford" },
+    function() { return "drive"}
 ];
+
+// Arrays can embed objects and objects can embed arrays
