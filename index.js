802f57525974051f8cd6e93b2d9e49eca71087df
lukachochua <lukachochua@gmail.com>
2023-03-14 20:19:57 +0400

    Episode 17: member creation, assignment and deletion

--- index.js
+++ index.js
@@ -1,5 +1,4 @@
-// Computed Member Access
-// Computed member access is an alternative to the dot syntax. This operator uses brackets []. 
+// Member Creation, assignment and deletion
 
 let car = {
     make: "Volvo",
@@ -13,6 +12,36 @@ let car = {
     drive: function() { return "drive"}
 };
 
+// car = "string" | | You can reassign keys and values in the 'car' object. 
+
+car.make = "ford"; // You can also target specific key value pair in the object with dot syntax. 
+
+car.make += 200; // With other operators, you can even concatinate something to the existing value in the object.
+
+car["speed"] *= 2;
+
+console.log(car["speed"]); // you can use other operators i.e. multiplication, to change numeric values inside the object. 
+
+car.engine = { newengine: "newEngine"}; // You can even manipualte objects inside parent object by assigning new values. 
+
+car.drive = "drive"; // Newly assigning subroutines is also possible. 
+
+// How to assign new key
+
+car.model = "v60"; // This is how you create new symbol/memory pointer inside given object. 
+car.color; // You can also add keys without values, or with value of undefined/null.
+
+car.color = "red"; // You can't create new variable color to include it in the object. You need to use dot syntax to assign specific value to the specific key.
+
+car.stop = function() { return "stop"};  // You can even assign new method to the object.
+
+car.engine.stop = function() { return "stop engine"}; // You can assign methods to the sub objects inside the main one with this syntax. 
+
+// How do you delete certain members.
+
+car.color = null; // This doesn't remove the key, only makes value null, so this is not the way to go. 
+delete car.color; // This is the way to delete the symbol/memory link in the object. 
+delete car.stop; // To delete method. 
 
 let array = ["string",
     100, 
@@ -21,34 +50,4 @@ let array = ["string",
     function() { return "drive"}
 ];
 
-console.log(car.engine.pistons[0]); // This is an example of computed member access. Wha's returned is symbol link istead of a number type.  
-
-console.log(car.engine.pistons[0].maker); // Computed member access is used mainly on arrays.
-
-console.log(array[0]); // This will return not a number '0' but memory pointer 0 which is "string".
-
-console.log(array[4]()); // If you want to invoke function, as in dot syntax, here you also need to put parenthisis.
-
-console.log(array[1+1-2]); // When you have computer member access, whatever is inside, is treated as a javascript statement. This is the same as array[0].
-
-let a = 10; // In these kind of operations, you could even use variables. 
-
-console.log(array[a-5-2]); // In this case console will print out array[3], as a = 10 - 5 - 2 = 3. JS computes statement inside of bracket. 
-
-console.log(car["make"]); // You can use computed member access as an alternative to dot syntax. in that case, you need to call variable/value name 
-// as a string inside of brackets [].
-
-console.log(car["engine"]["pistons"][1]['maker']); // This can also be used, to get whatever is in an sub object or an sub array.
-
-console.log(car["drive"]()); // This way, you can still invoke subroutines. AGAIN - use parenthisis. Otherwise, you'll get function syntax and not subroutin itself.
-
-let pointer = "make"; // One can also target variable.
-
-console.log(car[pointer]); // This returns Volvo, as pointer variable is assigned to the string "make";
-
-let newPointer = "speed"; 
-
-console.log(car[newPointer]); // Another example. 
-
-console.log(car["en" + "gine"]); // You can also do all kinds of things like concationation. You have ability to mix and match between dot syntax and 
-// Computed Member Access.
+console.log(car.make);
