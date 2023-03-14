0d8f734d5193580cb2cd0484b4e8b74da3447356
lukachochua <lukachochua@gmail.com>
2023-03-14 22:08:13 +0400

    Episode 18: array modification

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Member Creation, assignment and deletion
+// Array Modification
 
 let car = {
     make: "Volvo",
@@ -12,42 +12,60 @@ let car = {
     drive: function() { return "drive"}
 };
 
-// car = "string" | | You can reassign keys and values in the 'car' object. 
 
-car.make = "ford"; // You can also target specific key value pair in the object with dot syntax. 
 
-car.make += 200; // With other operators, you can even concatinate something to the existing value in the object.
-
-car["speed"] *= 2;
-
-console.log(car["speed"]); // you can use other operators i.e. multiplication, to change numeric values inside the object. 
-
-car.engine = { newengine: "newEngine"}; // You can even manipualte objects inside parent object by assigning new values. 
-
-car.drive = "drive"; // Newly assigning subroutines is also possible. 
-
-// How to assign new key
-
-car.model = "v60"; // This is how you create new symbol/memory pointer inside given object. 
-car.color; // You can also add keys without values, or with value of undefined/null.
-
-car.color = "red"; // You can't create new variable color to include it in the object. You need to use dot syntax to assign specific value to the specific key.
-
-car.stop = function() { return "stop"};  // You can even assign new method to the object.
-
-car.engine.stop = function() { return "stop engine"}; // You can assign methods to the sub objects inside the main one with this syntax. 
-
-// How do you delete certain members.
-
-car.color = null; // This doesn't remove the key, only makes value null, so this is not the way to go. 
-delete car.color; // This is the way to delete the symbol/memory link in the object. 
-delete car.stop; // To delete method. 
-
-let array = ["string",
+let array = [
+    "string",
     100, 
     ["embed", 200],
     { car: "ford" },
     function() { return "drive"}
 ];
 
-console.log(car.make);
+/*
+array[0] = "string"; // to assign new value to the index of the array, you just need to reassign it like so.
+
+array[0] += " concat"; // of course, one also can concatinate new values to the existing string. 
+
+array[5] = "new string"; // be careful while using brackets. If you don't know how many indexes there are, you might add undifined/null indexes
+
+array[20] = 2020; // Adds index of 20 and 14 empty ones after index 5 (that exists). 'empty x 14" in console. 
+
+array['test'] = "test";
+array.test2 = "test2"; // Don't do these kind of things, because array will start to function like an object and indexes can't be used anymore. 
+why is this bad? arrays are supposed to be countable/numerable. This way, array loses one of it's main charesteristics.
+
+*/
+
+// Length property. It tells us how many elements are in the array.
+
+array[5] = "new value"; // Now array has 6 elements. you can check length of an array with 'array.length'.
+// All of the objects have '__proto__', that javascript language created. It has all of the methods that can modify data in array. These are prebuild methods.
+
+
+// Remove elements from the array
+
+array.shift(); // Use this when you want to get rid of the first element of the array.
+
+array.pop();  // Use this when you want to get rid of the last element of the array.
+
+
+// Add elements to the array
+
+array.unshift("string", 20, 20.22, function(){}, {}, []); // One can add any kind of element (primitive data, object or function) to the array like so.
+// This adds items right at the begining at the array. 
+
+array.push(200, 300, 'new value'); // One can add elements to the array like so. In both cases you can add one value, or as many values as you wish. 
+
+array.splice(2, 2); // First of you define which index you want to start splicing from (index 2 in this case), than define how many elements u want to splice (2). 
+
+array.splice(1, 0, "spliced string", 200, 20.31); // You write 0 in the predifined splice method as the second argument and add new elements after comma. 
+// We don't delete index '1' in this case, it just moves down and new elements are added in. 
+
+array.splice(2, 2, 'world200'); // In this case splice() method deletes two indexes of an array and adds a new one, or new ones from the index one
+// gives to the splice function as the first argument.  
+
+
+
+console.log(array); 
+
