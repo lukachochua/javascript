ffed68f07efd4ce0b633a019640ca2150ef01442
lukachochua <lukachochua@gmail.com>
2023-03-17 00:23:52 +0400

    Episode 23: this context

--- index.js
+++ index.js
@@ -1,38 +1,49 @@
-// Inferred Globals & Scope
-// Window object in JS is highest level object. All of the symbols/functions are of Global Scope. 
-// Only objects that are directly accessable from window object have global scope/are accessible from anywhere. 
+// 'This' context
 
-var engine = {
-    maker: "Ford",
-    headGasket: {
-        maker: 'Golf',
-        pots: [
-            "piston1", // To access pots you need to go to the 'engine', use dot syntax and go to the 'headGasket'.'pots' and than use bracket syntax [0]. 
-            "piston2"
-        ]
-    }
+// This word is a context based word - it has a reference. It needs a point of what one's looking at. 
+// When you open your brower window, there is an environment that is established - window object that includes all of the default properties and methods of JS.
+
+console.log(this); // -> this by default refers to the window object. 
+
+var object = {
+    prop: this,
+    embed:
+    {
+        embed: true,
+        method: function () { return this; } // now 'this' refers to the 'embed' object. curly braces are it's context. 
+    },
+    method: function() {return this;} //This pointer is going out of the pointer by one and refers to the object that cointains it. 
 };
 
-function runExpression() // Functions have their own scope/execution context, which are defined by curly braces. 
-{
-    var a = 10;
+var array = [
+    this,
+    function () { return this; } // Same with arrays, as arrays are type of objects. 
+];
 
-    function add() {
-        var engine = "engine"; // if we create variable called engine inside of the FIRST inferred scope, it's value will be returned. 
-        // But engine object is still available on global scope. To be blunt - each one of the braces has it's scope. 
+// if you have 'this' keyword in an object, or an array, it's always pointing to the window object. 
+// Only callable objects allow us to change the context of the 'this'.
 
-        // engine = "New String"; This way because of the assignment operator, it assigns new value to the global scoped variable 'engine'. 
-        // You need to be careful with it during coding. To create variable within a scope you need to use 'var', 'let', or 'const' declarations. 
-        test = "New String";  // because we use assignment operator, 'test' still has memory pointer on the window object, despite
-        // not being explicitly declared as a variable. This is called clubbering a global variables and is bad practice. which is unnecessary. 
+function global() {
+    console.log('from global', this);
 
-        test; // without assignment operator JS will return an error as it can't find tests value. 
-        
-        console.log(engine); // JS is trying to deduce where the memory pointer 'engine' is. first it goes to the curly brackets
-        // inside of the add() function and searches for it there, than in 'runExpression()' and only than in Global/Window Scope. 
-    }
+    function sub() { console.log('from sub', this)};
 
-    add();
-
+    sub(); // by default callable objects go to the global scope.
+    
+    // return this; // In this case it steps out once to the outer scope and reaches window {} context. 
+    // function add() { this; }  // In this case, even in embeded object/method 'this' refers to the window. This it how functions differ from arrays and objects in
+    // the given context. 
 }
 
+
+
+global.call(object); // We use dot syntax to use 'call' function on a callable object. This, once again, proves that functions/methods are objects too. 
+// This is a way to change the context of the 'this' keyword. In the console an object will be returned that contains values of the 'var object'.
+
+new global(); // with 'new' keywoard, you're creating blank object. 
+
+// You can use call method on a callable method and pass in ar argument and that argument will be assigned to the 'this' context. 
+
+/* So, if function is directly invoked, 'this' will refer to the object it's called from. If you use call method, 'this' will refer to the argument of the call 
+function. Also, you have new keyword, in which case, it creates new context. 
+*/ 
