95c03a3223e5dd8f6103b163c7bdbb3e6343083c
lukachochua <lukachochua@gmail.com>
2023-03-17 17:57:53 +0400

    Episode 24: constructors

--- index.js
+++ index.js
@@ -1,49 +1,20 @@
-// 'This' context
+// Constructors
 
-// This word is a context based word - it has a reference. It needs a point of what one's looking at. 
-// When you open your brower window, there is an environment that is established - window object that includes all of the default properties and methods of JS.
+// Constructors are like construction workers. JS has constructor functions that provide blueprint for the objects.
 
-console.log(this); // -> this by default refers to the window object. 
+// Apple example - In game we have a player(object) that goes through the game and collects apples, that are also objects. 
+// We need constructor to dynamically create an apple - new instance of the object - copy of the blueprint. 
 
-var object = {
-    prop: this,
-    embed:
-    {
-        embed: true,
-        method: function () { return this; } // now 'this' refers to the 'embed' object. curly braces are it's context. 
-    },
-    method: function() {return this;} //This pointer is going out of the pointer by one and refers to the object that cointains it. 
-};
-
-var array = [
-    this,
-    function () { return this; } // Same with arrays, as arrays are type of objects. 
-];
-
-// if you have 'this' keyword in an object, or an array, it's always pointing to the window object. 
-// Only callable objects allow us to change the context of the 'this'.
-
-function global() {
-    console.log('from global', this);
-
-    function sub() { console.log('from sub', this)};
-
-    sub(); // by default callable objects go to the global scope.
-    
-    // return this; // In this case it steps out once to the outer scope and reaches window {} context. 
-    // function add() { this; }  // In this case, even in embeded object/method 'this' refers to the window. This it how functions differ from arrays and objects in
-    // the given context. 
+function Apple(x, y, color, score) // add capital letter to the constructor functions. Just a naming convention to understand that function is constructor.
+{
+    this.x = x;
+    this.y = y;
+    this.color = color;
+    this.score = score;
+    // You can say return this;, but once we start invoking this execution context, it'll automatically return the values.  
 }
 
-
-
-global.call(object); // We use dot syntax to use 'call' function on a callable object. This, once again, proves that functions/methods are objects too. 
-// This is a way to change the context of the 'this' keyword. In the console an object will be returned that contains values of the 'var object'.
-
-new global(); // with 'new' keywoard, you're creating blank object. 
-
-// You can use call method on a callable method and pass in ar argument and that argument will be assigned to the 'this' context. 
-
-/* So, if function is directly invoked, 'this' will refer to the object it's called from. If you use call method, 'this' will refer to the argument of the call 
-function. Also, you have new keyword, in which case, it creates new context. 
-*/ 
+var apple1 = new Apple(10, 20, 'red', 200); // Once you create this object, there is no way to reference it. So you assign it to the symbol name. 
+var apple2 = new Apple(10, 20, 'green', 60);
+var apple3 = new Apple(10, 20, 'yellow', 20); // even all thre of the apples come from one factory, they're all different objects with their own values 
+// - different instances. Each of the obejcts has same structure, but different values. 
