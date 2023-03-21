829154021270374ced4f4f1f64eb22a05e05d90a
lukachochua <lukachochua@gmail.com>
2023-03-21 21:18:23 +0400

    Episode 34: changing elements style

--- index.js
+++ index.js
@@ -1,25 +1,25 @@
-// Changing Elements Contents
+// Changing Elements Style
 
-// If you have large JS file at the end of HTML content (last part of body), not to ruin websites load time.
-// It also means that script is ready to modify all of the HTML elements. 
-
-var pHello = document.getElementById("hello"); // Not a new object, just a memory pointer to an existing object created in the Document Object.
-
-// You can name 'getElement' and save it to the memory pointer a.k.a. variable.
-
-pHello.innerText = "New World"; // innerText contain no HTML whatsoever.
-pHello.innerHTML += " <b> Order</b>"; // While using innerHTML one can change HTML too. 
-pHello.outerHTML = '<h2 id="hello">New World Order <span>Hello World</span></h2>' // There is also outerHTML function that gives us an html of the parent element. <p></p> in this case.
+var el = document.getElementById('style'); // First you target the element. Than we target elements 'style' property/object. 
+// That object contains many properties resembling css properties and than we assign values as in CSS properties. 
 
 /*
-document.getElementById("hello").innerText += " Order"; // Used in console returns 'New World Order' on the browser
-First part returns an object, after the second '.' it's accessing an object. 
+el.style.background = "blue"; // This is an object in the Document Object Model. it has a lot of properties that allow us to modify css. 
+// background is just one of them. Here we need quotation marks - we don't need variable blue, but string blue.
+el.style.color = "white";
+el.style.width = "200px"; // You need to include measurment unit like "px".
+*/ 
+
+/*
+
+el.style.cssText = "background: blue; color:white; width:200px"; // This is a better and more efficient way to style html element
+el.style.cssText += "height:100px;"; // You can also concatinate styles not to lose existing ones.
+// csstext is basically applied to the style attribute.
+
 */
 
+// To get existing style from the <style> tag, you can use 
 
-var spanH1 = document.querySelectorAll('h1 span');//[0]  querySelectorAll() always returns an array and html part of the element is always [0] index.
+console.log(getComputedStyle(el)); // property.
 
-spanH1[0].innerHTML = "new text here!!"; // You can manipulate array at this part too.
-
-// When we say document.getElementsByTagName and get all paragraph elements,  You're getting memory pointers to the object in the document model. 
-// Not a new model, just a way to reference existing model. 
+// Computed properties are default, they're not actual styles applied from JS. 
