ace7ac0602faa97bd7610c7bbc5ce61112532c51
lukachochua <lukachochua@gmail.com>
2023-03-27 22:58:51 +0400

    Episode 37: final project

--- index.js
+++ index.js
@@ -1,16 +1,21 @@
-// Create and appendChild & insertBefore methods
+// Final Project
 
-var element = document.createElement('div'); // We create an variable called element with document.createElement function using 'div' as a parameter. 
+function set()
+{
+    let elements = document.getElementsByName('cssProperty');
 
-element.style.cssText = "width:200px; height:20px; background:blue;"; // We styled it directly from JS.
+    for (index = 0; index < elements.length; index++)
+    {
+        let cssProperty = elements[index].getAttribute('id');
+        let cssValue = elements[index].value;
 
-element.onclick = function(){alert('hello');}; // and wrote an onclick event for the 'element' variable, which is represented as an div object.
+        let div = document.getElementById('modify');
 
+        div.style[cssProperty] = cssValue;
+        console.log(cssProperty);
+        console.log(cssValue);
+    }
+}
 
-// document.body.appendChild(element); // We target body part of an html document and with appendChild(parameter) appended JS code in an html.
-// Appended div comes right at the bottom, as script tag is located/ran there.
+document.getElementById('set').addEventListener('click', set);
 
-// Node is a part of the overall structure.
-
-var target = document.getElementById('yellow'); // We create a target using document.getElementById() method
-document.body.insertBefore(element, target); // and with an instertBefore() method we put an 'element' before div with an ID of 'yellow'/
